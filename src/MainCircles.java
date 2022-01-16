4d96f893c3ad44436951d91be24513ee3f38f0f7
Dejni Muka <dejnimuka@gmail.com>
2022-01-16 19:33:21 +0100

    Update MainCircles.java

--- src/MainCircles.java
+++ src/MainCircles.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.Dimension;
 import java.util.Random;
+import java.lang.Math.*;
 
 public class MainCircles extends JFrame {
 	
@@ -22,16 +23,16 @@ public class MainCircles extends JFrame {
     int upperbound = 1000;
     int centreCoord = centre.nextInt(upperbound);
     
-    //int widthHeight = 500;
-    //int circleLength = length.nextInt(widthHeight);
-
+    int upperLength = 500, lowerLength = 10;
+    int length = (int) (Math.random() * (upperLength - lowerLength)) + lowerLength;//exclusive to upper, inclusive to lower, gets random
+    																			   //values between lowerLength and upperLength
     @Override
     public void paint(Graphics g) 
     {
         Graphics2D g2d = (Graphics2D) g;
         g2d.setColor(new Color(125, 167, 116));//sets the color which will be used to fill the circles
-        g2d.drawOval(centreCoord, centreCoord, 200, 200);//draws the circle
-        g2d.fillOval(centreCoord, centreCoord, 200, 200);//fills the circle with color
+        g2d.drawOval(centreCoord, centreCoord, length, length);//draws the circle
+        g2d.fillOval(centreCoord, centreCoord, length, length);//fills the circle with color
     }
 
     public static void main(String[] args) 
